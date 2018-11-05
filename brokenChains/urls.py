9ad4c6365ec397bf744018d9b43e0c09b53c50ae
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-11-05 19:24:29 +0100

    Changes: - Created `HabitList` view - wired to the urls - added the `ensure_csrf_token` decorator to make sure CSRFToken is sent with the request.

--- brokenChains/urls.py
+++ brokenChains/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 
 
 urlpatterns = [
+	path('habits/', views.HabitList.as_view(), name='habits-list'),
 ]
--- brokenChains/views.py
+++ brokenChains/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils.decorators import method_decorator
+from rest_framework import generics, permissions
+from .models import Habit, Session
+from .serializers import HabitSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# Create your views here.
+
+
+
+
+
+class HabitList(generics.ListCreateAPIView):
+	queryset = Habit.objects.all()
+	serializer_class = HabitSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+	def perform_create(self, serializer):
+		serializer.save(owner=self.request.user)
+
+	@method_decorator(ensure_csrf_cookie)
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
+
+
+
+
+
+
+
+
