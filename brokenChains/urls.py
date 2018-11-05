b5e543e41294bfe89ef162fc06b5bda0bb4679f9
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-11-05 21:27:59 +0100

    Created `SessionList` view and registered the route

--- brokenChains/urls.py
+++ brokenChains/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
 	path('habits/', views.HabitList.as_view(), name='habits-list'),
 	path('habits/<int:pk>/', views.HabitDetail.as_view(), name='habit-detail'),
+	path('sessions/', views.SessionList.as_view(), name='sessions-list'),
 ]
--- brokenChains/views.py
+++ brokenChains/views.py
@@ -3,13 +3,10 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.utils.decorators import method_decorator
 from rest_framework import generics, permissions
 from .models import Habit, Session
-from .serializers import HabitSerializer
+from .serializers import HabitSerializer, SessionSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-
-
-
 
 
 
@@ -36,6 +33,11 @@ class HabitDetail(generics.RetrieveDestroyAPIView):
 		return self.destroy(request, *args, **kwargs)
 
 
+class SessionList(generics.ListCreateAPIView):
+	queryset = Session.objects.all()
+	serializer_class = SessionSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
-
-
+	@method_decorator(ensure_csrf_cookie)
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
