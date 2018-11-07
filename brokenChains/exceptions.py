dba8a62af5895b5d4e3c5dad9645da4e4a845979
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-11-07 19:18:27 +0100

    created a special class `UniqueTogetherValidationError` that could be raised for when `unique_together` validation fails.

---
+++ brokenChains/exceptions.py
@@ -0,0 +1,17 @@
+from rest_framework.exceptions import APIException
+from django.utils.encoding import force_text
+from rest_framework import status
+
+
+
+class UniqueTogetherValidationError(APIException):
+	status_code = status.HTTP_403_FORBIDDEN
+	default_detail = '403 Forbidden.'
+
+	def __init__(self, detail, field, status_code):
+		if status_code is not None:
+			self.status_code = status_code
+		if detail is not None:
+			self.detail = {field: force_text(detail)}
+		else:
+			self.detail = {'detail': force_text(self.default_detail)}
