10b8ccc656c5562a11e3088d4e6b2e43bc293f93
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-11-11 20:56:02 +0100

    Added UserSerializer

--- brokenChains/serializers.py
+++ brokenChains/serializers.py
@@ -17,3 +17,21 @@ class HabitSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Habit
 		fields = ('url', 'owner',  'name', 'goal', 'start_date', 'stop_date', 'sessions')
+
+
+class UserSerializer(serializers.ModelSerializer):
+	habits = HabitSerializer(many=True, read_only=True)
+
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password')
+		extra_kwargs = {'password': {'write_only': True}}
+
+	def create(self, validated_data):
+		user = User(
+			email=validated_data['email'],
+			username=validated_data['username'],
+		)
+		user.set_password(validated_data['password'])
+		user.save()
+		return user
