49a0b83bc25cf8defbdb8e47c67e32ed7d054980
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-11-11 21:07:44 +0100

    Manual authentication token generation added to admin site.

--- brokenChains/admin.py
+++ brokenChains/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Habit, Session
+from rest_framework.authtoken.admin import TokenAdmin
 
 
 
+TokenAdmin.raw_id_fields = ('user',)
 admin.site.register(Habit)
 admin.site.register(Session)
