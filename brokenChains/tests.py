d6622e5445fcd55a9376dfd77eaab061444a7134
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-11-14 19:17:32 +0100

    create habit test

--- brokenChains/tests.py
+++ brokenChains/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth .models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
 from .models import Habit, Session
 
@@ -9,6 +10,9 @@ from .models import Habit, Session
 
 class UserRegistrationTest(APITestCase):
 	def test_user_registration(self):
+		"""
+		Test user can register.
+		"""
 		data = {
 			'username': 'testUser',
 			'email': 'testEmail@mail.com',
@@ -28,6 +32,30 @@ class UserAuthenticationTests(APITestCase):
 		}
 
 	def test_get_user_auth_token(self):
+		"""
+		Test user can get authentication token.
+		"""
 		url = reverse('get-auth-token')
 		response = self.client.post(url, self.data)
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class CreateHabitTest(APITestCase):
+	def setUp(self):
+		self.user = User.objects.create_user('testUser', 'testEmail@email.com', 'testPassword')
+		self.client.login(username='testUser', password='testPassword')
+		self.url = reverse('habits-list')
+		self.data = {
+			'name': 'study',
+			'goal': 'to be literate.',
+			'stop_date': '2018-12-30'
+		}
+
+	def test_can_create_habit(self):
+		"""
+		Test user can create new habit.
+		"""
+		response = self.client.post(self.url, self.data)
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
