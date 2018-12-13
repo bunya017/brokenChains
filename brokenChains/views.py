f4cd0760f33a0b60f6dbd4e89040a5b3667d7824
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-12-14 00:04:22 +0100

    Authentication token is being returned upon user registeration.

--- brokenChains/views.py
+++ brokenChains/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.utils.decorators import method_decorator
-from rest_framework import generics, permissions, serializers
+from rest_framework import generics, permissions, serializers, status
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken as DRFObtainAuthToken
-from rest_framework.exceptions import NotAuthenticated, PermissionDenied
+from rest_framework.exceptions import APIException, NotAuthenticated, PermissionDenied
 from rest_framework.response import Response
 from .models import Habit, Session
 from .serializers import HabitSerializer, SessionSerializer, UserSerializer
@@ -101,6 +101,21 @@ class ObtainAuthToken(DRFObtainAuthToken):
 class UserRegistration(generics.CreateAPIView):
 	serializer_class = UserSerializer
 	permission_classes = (permissions.AllowAny,)
+
+	def perform_create(self, serializer):
+		data = serializer.validated_data
+		email = data['email']
+		if User.objects.filter(email=email).exists():
+			raise serializers.ValidationError('A user with that email already exists.')
+		serializer.save()
+
+	def create(self, request, *args, **kwargs): # <- here i forgot self
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		self.perform_create(serializer)
+		headers = self.get_success_headers(serializer.data)
+		token, created = Token.objects.get_or_create(user=serializer.instance)
+		return Response({'token': token.key}, status=status.HTTP_201_CREATED, headers=headers)
 
 	@method_decorator(ensure_csrf_cookie)
 	def post(self, request, *args, **kwargs):
